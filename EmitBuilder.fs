57d939f29779ced626265e04cc60fbea4c22b819
Marcus van Houdt <marcus.vanhoudt@serialssolutions.com>
2012-11-28 09:56:13 +0000

    Initial commit.

---
+++ EmitBuilder.fs
@@ -0,0 +1,82 @@
+﻿module EmitUtils
+
+open System
+open System.Reflection
+open System.Reflection.Emit
+
+type EmitOp =
+   | Call of MethodInfo
+   | Label of Label
+   | Goto of Label
+
+type EmitBuilder(ilgen: ILGenerator) =
+   member this.ILGen = ilgen
+   member this.Yield (c: EmitOp) = 
+      match c with 
+      | Call m  -> ilgen.Emit(OpCodes.Callvirt, m)
+      | Label l -> ilgen.MarkLabel(l)
+      | Goto l  -> ilgen.Emit(OpCodes.Br, l)
+
+   member this.Yield (u:unit) = u
+   member this.Yield (o: OpCode) = ilgen.Emit(o)
+   member this.Yield (inp: OpCode * int32) = match inp with (a, b) -> ilgen.Emit(a, b)
+   member this.Yield (inp: OpCode * string) = match inp with (a, b) -> ilgen.Emit(a, b)
+   member this.Yield (inp: OpCode * LocalBuilder) = match inp with (a, b) -> ilgen.Emit(a, b)
+   member this.Yield (inp: OpCode * Type) = match inp with (a, b) -> ilgen.Emit(a, b)
+   member this.Yield (inp: OpCode * FieldInfo) = match inp with (a, b) -> ilgen.Emit(a, b)
+   member this.Yield (inp: OpCode * MethodInfo) = match inp with (a, b) -> ilgen.Emit(a, b)
+   member this.Yield (inp: OpCode * Label) = match inp with (a, b) -> ilgen.Emit(a, b)
+
+   member this.Delay (x: unit -> unit) = x
+   member this.Run x = x()
+
+   // e1;e2 translates to something like b.Combine(e1, Delay(e2)) and Delay in our case returns a function.
+   member this.Combine (x, y) = y()
+
+   member this.Zero (u:unit) = ()
+
+   member this.TryFinally (body: unit -> unit, final: unit -> unit) = 
+      try
+         ilgen.BeginExceptionBlock() |> ignore
+         body()
+      finally
+         ilgen.BeginFinallyBlock()
+         final()
+         ilgen.EndExceptionBlock()
+    
+   member this.For (exp: 'a * 'b, body: LocalBuilder -> unit) =
+      let start = ilgen.DefineLabel()
+      let finish = ilgen.DefineLabel()
+      let loopVar = ilgen.DeclareLocal(typeof<int32>)
+      let (from, length) = exp
+      this {
+         match box from with
+         | :? int32 as i -> ilgen.Emit(OpCodes.Ldc_I4, i) // todo: optimize into ldc_i4_0 etc.
+         | :? LocalBuilder as l -> ilgen.Emit(OpCodes.Ldloc, l)
+         | _ -> failwith "invalid from type"
+
+         yield OpCodes.Stloc, loopVar
+   
+         yield Label(start)
+
+         yield OpCodes.Ldloc, loopVar
+
+         // load length
+         match box length with
+         | :? int32 as i ->        yield OpCodes.Ldc_I4, i // todo: optimize into ldc_i4_0 etc.
+         | :? LocalBuilder as l -> yield OpCodes.Ldloc, l
+         | _ -> failwith "invalid length type"
+
+         yield OpCodes.Ceq             // pop2 , load 0 or 1
+         yield OpCodes.Brtrue, finish
+   
+         body loopVar
+
+         yield OpCodes.Ldloc, loopVar
+         yield OpCodes.Ldc_I4_1        // push 1
+         yield OpCodes.Add             // add 1 to array length
+         yield OpCodes.Stloc, loopVar
+         yield OpCodes.Br, start       // goto next entry
+
+         yield Label(finish)
+      }
---
+++ Tests/DynamicMethodTest.fs
@@ -0,0 +1,29 @@
+﻿module DynamicMethodTest
+
+open System.Reflection.Emit
+open Xunit
+open EmitUtils
+
+type HelloWorldDelegate = delegate of int32 -> string
+
+[<Fact>]
+let DynamicMethodTest() =
+   let meth = new DynamicMethod("HelloWorld", typeof<string>, [| typeof<int32> |])
+   let asm = new EmitBuilder(meth.GetILGenerator())
+   asm {
+         let count = asm.ILGen.DeclareLocal(typeof<int32>)
+         yield OpCodes.Ldarg_0  // load count
+         yield OpCodes.Stloc, count
+         
+         yield OpCodes.Ldstr, ""
+
+         for i in (0, count) do
+            yield OpCodes.Ldstr, "Hello World! "
+            yield OpCodes.Call, typeof<string>.GetMethod("Concat", [| typeof<string>; typeof<string> |])
+
+         yield OpCodes.Ret
+       }
+   let d = meth.CreateDelegate(typeof<HelloWorldDelegate>) :?> HelloWorldDelegate
+   Assert.Equal<string>("", d.Invoke(0))
+   Assert.Equal<string>("Hello World! ", d.Invoke(1))
+   Assert.Equal<string>("Hello World! Hello World! ", d.Invoke(2))
